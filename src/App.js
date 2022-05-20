a23acef071105671b0170ecd800722abbbdbbf9b
Nausheen Akhter <nausheen701@gmail.com>
2022-05-20 11:57:29 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import './App.css'
+import React, { useEffect, useState } from 'react'
 
 const API_URL = 'https://thecatapi.com?api_key=04075186-a95b-4d78-9169-42ecc5a9b6ef'
 
-
-
 const App = () => {
+  const [cats, setCats] =  useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
   
 
   return (
