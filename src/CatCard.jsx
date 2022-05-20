6e3c6d0aacab26f33d1d79abdab831a3c8077363
Nausheen Akhter <nausheen701@gmail.com>
2022-05-20 12:05:55 +0300

    catcard

---
+++ src/CatCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CatCard = ({ cat: { name, temperament, origin, description } }) => {
+    return (
+        <div className="cat"></div>
+        )
+    }
+    
+    export default CatCard
