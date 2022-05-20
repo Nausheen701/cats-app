33f8b3280a3c9a4d72dc61ebf42888d55a7b369c
Nausheen Akhter <nausheen701@gmail.com>
2022-05-20 12:09:51 +0300

    populate catcard

--- src/CatCard.jsx
+++ src/CatCard.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const CatCard = ({ cat: { name, temperament, origin, description } }) => {
     return (
-        <div className="cat"></div>
+        <div className="cat">
+            <div>
+          <span>{name}</span>
+          </div>
+          <div>
+            <img src={name.Poster !== 'N/A' ? name.Poster : 'https://via.placeholder.com/400'} />
+          </div>
+          <div>
+            <span>{name}</span>
+            {/* <h3>{cat.breed}</h3> */}
+          </div>
+        </div>
         )
     }
     
